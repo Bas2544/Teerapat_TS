4118bbbd1e037d12c79be70656bc629c2b0fcb2c
Bas2544 <thebasza.2544@gmail.com>
2021-11-23 14:27:28 +0700

    first commit

---
+++ src/YourName_Q1.ts
@@ -0,0 +1,24 @@
+let workYear : number[] = [1,2,3,4,5,6];
+let commission: number[]=[];
+
+workYear.forEach((s)=>){
+    
+}
+
+interface Human{
+    id: number,
+    name: string,
+    workYear: number,
+    salary: number,
+}
+const employee : Humen ={
+    id : 1004,
+    name: "Teerapat",
+    workYear:1,
+}
+
+class employee implements Humen{
+    id: number
+    name : string;
+    workYear :;
+}
